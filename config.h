d4e5be7c65b719942a9efa29ef32d3d3f22665ac
Osama Ragab <theosamaragab@gmail.com>
2023-09-04 03:02:48 +0300

    Revert "apply center patch"
    This reverts commit d397e8c603bdcca74fb6b5d69b837b3935351cb6.

--- config.h
+++ config.h
@@ -3,8 +3,6 @@
 
 static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom */
 static int fuzzy = 0;       /* -F  option; if 1, dmenu uses fuzzy matching */
-static int centered = 0;    /* -c  option; centers dmenu on screen */
-static int min_width = 500; /* minimum width when centered */
 static char *prompt = NULL; /* -p  option; prompt to the left of input field */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
