f45e95a5757b41b4701ae7dde452528d3bd1b431
Osama Ragab <theosamaragab@gmail.com>
2023-09-13 01:00:02 +0300

    Revert "apply fuzzymatch patch"
    This reverts commit 302d32927303ec73399a0dd26dcb8bf6aee89edb.

--- config.h
+++ config.h
@@ -2,7 +2,6 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom */
-static int fuzzy = 0;       /* -F  option; if 1, dmenu uses fuzzy matching */
 static char *prompt = NULL; /* -p  option; prompt to the left of input field */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
